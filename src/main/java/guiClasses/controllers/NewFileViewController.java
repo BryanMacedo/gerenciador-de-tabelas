8d020aa2ee3060273463e834b3f295d3bf0602a3
Bryan Macedo <contato.bryansm@hotmail.com>
2025-04-18 18:04:17 -0300

    Arrumei a sobreposição dos elementeos e comecei a verificar se existe a pasta que será usada para guardar os arquivos .xlsx.

--- src/main/java/guiClasses/controllers/NewFileViewController.java
+++ src/main/java/guiClasses/controllers/NewFileViewController.java
@@ -5,10 +5,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class NewFileViewController implements Initializable {
@@ -22,26 +31,42 @@ public class NewFileViewController implements Initializable {
     private ImageView imgvMinimize;
 
     @FXML
-    private void onImgvCloseClick(){
+    private TextField tfFileName;
+
+    @FXML
+    private void onImgvCloseClick() {
         Platform.exit();
     }
 
     @FXML
-    private void onImgvMinimizeClick(){
+    private void onImgvMinimizeClick() {
         Stage stage = (Stage) imgvMinimize.getScene().getWindow();
         stage.setIconified(true);
     }
 
     @FXML
-    private void onBtCreateAction(){
-        /*
-        * ao clciar veirificar se tem uma psata chamada tables-GT no disco C
-        * Caso n tenha criar pelo java
-        * após isso verificar se n existe um arquivo com o mesmo nome q o usuário informou
-        * se n existir criar o arquivo excel
-        * se existir mostrar um aviso e n criar*/
-    }
+    private void onBtCreateAction() {
+        Path path = Paths.get("C://tabelas-GT");
+        String fileName = null;
 
+        if (!Files.exists(path) && !Files.isDirectory(path)) {
+            try {
+                Files.createDirectory(path);
+            } catch (IOException e) {
+                System.out.println("Pasta não criada." + e.getMessage());
+            }
+        }
+
+        if (!tfFileName.equals("")) {
+            fileName = tfFileName.getText() + ".xlsx";
+        } else {
+            System.out.println("Por favor digíte um nome para o arquivo!");
+        }
+        System.out.println("O usuário digítou: " + fileName);
+
+        String fullPath = path + "\\" + fileName;
+
+    }
 
 
     @Override
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.bryanmacedo.projetogerenciadordetabelas {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.poi.ooxml;
 
 
     opens guiClasses.controllers to javafx.fxml, javafx.graphics;
