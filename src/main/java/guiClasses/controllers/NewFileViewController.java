263a1bbae03f2a826849b9be3e1f50847e88ee24
Bryan Macedo <contato.bryansm@hotmail.com>
2025-04-19 12:43:23 -0300

    Comecei a criar arquivos .xlsx com o nome digitado pelousuário.

--- src/main/java/guiClasses/controllers/NewFileViewController.java
+++ src/main/java/guiClasses/controllers/NewFileViewController.java
@@ -8,10 +8,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -46,8 +48,43 @@ public class NewFileViewController implements Initializable {
 
     @FXML
     private void onBtCreateAction() {
-        Path path = Paths.get("C://tabelas-GT");
+        Path path = Paths.get("C:\\tabelas-GT");
         String fileName = null;
+        fileName = tfFileName.getText().trim();
+
+        if (fileName.isEmpty()) {
+            System.out.println("Por favor digíte um nome para o arquivo!");
+            return;
+        }
+
+
+        //verificando se já existe um arquivo com o nome informado
+        String fileNameXlsx = fileName + ".xlsx";
+
+        File checkFile = new File(path.toString(), fileNameXlsx);
+        if (checkFile.exists()) {
+            System.out.println("Já existe um arquivo com este nome, por favor digite outro.");
+            tfFileName.clear();
+        } else {
+            // criando o arquivo
+            Path fullPath = Paths.get(path.toString(), fileName + ".xlsx");
+
+            try (XSSFWorkbook workbook = new XSSFWorkbook();
+                 FileOutputStream fileOutputStream = new FileOutputStream(fullPath.toString())) {
+                workbook.createSheet(fileName);
+                workbook.write(fileOutputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //tirar a criação da pasta daqui quando tiver a tela inicial
+        Path path = Paths.get("C://tabelas-GT");
 
         if (!Files.exists(path) && !Files.isDirectory(path)) {
             try {
@@ -56,20 +93,5 @@ public class NewFileViewController implements Initializable {
                 System.out.println("Pasta não criada." + e.getMessage());
             }
         }
-
-        if (!tfFileName.equals("")) {
-            fileName = tfFileName.getText() + ".xlsx";
-        } else {
-            System.out.println("Por favor digíte um nome para o arquivo!");
-        }
-        System.out.println("O usuário digítou: " + fileName);
-
-        String fullPath = path + "\\" + fileName;
-
-    }
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 }
