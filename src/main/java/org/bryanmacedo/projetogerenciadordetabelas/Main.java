9131cc7f62e3da13cbedc0afc6fec4162000af8d
Bryan Macedo <contato.bryansm@hotmail.com>
2025-05-05 14:15:47 -0300

    Adicionei um icone no app e troquei o título.

--- src/main/java/org/bryanmacedo/projetogerenciadordetabelas/Main.java
+++ src/main/java/org/bryanmacedo/projetogerenciadordetabelas/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -17,9 +18,12 @@ public class Main extends Application {
         Scene scene = new Scene(root, 1445, 833);
         scene.setFill(Color.TRANSPARENT);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setTitle("Test");
+        primaryStage.setTitle("Game Tables");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        Image icon = new Image(getClass().getResourceAsStream("/imgs/imgAppIcon/ic_app_icon.png"));
+        primaryStage.getIcons().add(icon);
     }
 
     public static void main(String[] args) {
